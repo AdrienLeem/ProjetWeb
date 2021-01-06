8a8cf57f173dcc6a40e17fdd2c4440ad209b9a62
Gauthier <go.hembise@gmail.com>
2021-01-06 11:59:35 +0100

    correction bug Register/Login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
         $request->validate([ // Conditions pour que le formulaire accepte les données
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
+            'password' => 'required|string|min:6|confirmed',
             'password_confirmation' => 'required',
         ]);
 
